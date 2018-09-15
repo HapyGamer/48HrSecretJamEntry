1338f8dbcfcdfcc2c653a3a043b9e5d4c79766a6
Callum Scholes <hapymailer74@gmail.com>
2018-09-15 10:37:34 +1000

    Created basic movement and terrain

---
+++ Immortal Protector/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public float smoothing = 3;
+
+	private Transform player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		offset = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		transform.position = Vector3.Lerp(transform.position, player.position + offset, smoothing);
+	}
+}
---
+++ Immortal Protector/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed; //get speed
+	public float groundDistance; //get distance from player to ground
+	public LayerMask ground; //what layer is ground
+	public float jumpHeight; //what is jump height
+
+	private CharacterController controller;
+	private float Gravity = Physics.gravity.y;
+	private Vector3 velocity;
+	private bool isGrounded = true;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		controller = GetComponent<CharacterController>(); //get character controller
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));//x z movement
+		controller.Move(move * Time.deltaTime * speed);//move in x or z direction
+		if (move!= Vector3.zero)
+		{
+			transform.forward = move;//turn if not facing right way
+		}
+
+		velocity.y += Gravity * Time.deltaTime;//add gravity to velocity (could just be a single variable) (no gravity in character controller)
+
+		controller.Move(velocity * Time.deltaTime); // make character fall due to gravity
+
+		isGrounded = Physics.CheckSphere(transform.position, groundDistance, ground, QueryTriggerInteraction.Ignore); // check if on ground
+		
+
+		if (isGrounded && velocity.y < 0)
+		{
+			velocity.y = 0f;//if on ground then no gravity
+		}
+
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			velocity.y += Mathf.Sqrt(jumpHeight * -2f * Gravity);//better jumping
+		}
+	}
+}
