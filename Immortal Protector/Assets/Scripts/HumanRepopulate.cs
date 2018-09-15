ca9f8386feb01dacc8fd22d7de12acbb24d75826
Callum Scholes <hapymailer74@gmail.com>
2018-09-15 14:59:10 +1000

    quickly pushing for test

--- Immortal Protector/Assets/Scripts/HumanRepopulate.cs
+++ Immortal Protector/Assets/Scripts/HumanRepopulate.cs
@@ -10,33 +10,95 @@ using UnityEngine.AI;
 /// </summary>
 public class HumanRepopulate : MonoBehaviour {
 
-	public float reproduceDistance;
-	public float reproduceDuration;
-	public float reproduceSightDistance;
+	public GameObject baby;
 
-	public Vector2 reproductionAgeRange;
+	public float r_Distance;
+	public float r_Duration;
+	public float r_SightDistance;
+
+	public Vector2 r_AgeRange;
 
 	private bool canReproduce = false;
+	private bool grownUp = false;
+
+	private float r_Timer;
+	private float r_CoolDown;
+	private int currentBabies;
+
 	private Transform mate;
+	private HumanCurrentStats m_Stats;
 	private HumanCurrentStats c_Stats;
+	private NavMeshAgent agent;
+	private PopulationManager population;
 
 	// Use this for initialization
 	void Start ()
 	{
-		c_Stats = GetComponent<HumanCurrentStats>();			
+		c_Stats = GetComponent<HumanCurrentStats>();
+		agent = GetComponent<NavMeshAgent>();
+		population = FindObjectOfType<PopulationManager>().GetComponent<PopulationManager>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
-	{
-		if (c_Stats.c_Age >= reproductionAgeRange.x && c_Stats.c_Age < reproductionAgeRange.y && canReproduce)
+	{	
+		if (c_Stats.c_Age >= r_AgeRange.x && c_Stats.c_Age < r_AgeRange.y && canReproduce)//check if within age range and mate nearby
 		{
-			Reproduce(mate.position);
+
+			if (!grownUp)
+			{
+				CheckIfPartnerIsNear();
+				grownUp = true;
+			}
+
+			//if not within reproduciton range go to them
+			if (Vector3.Distance(transform.position, mate.position) > r_Distance)
+			{
+				agent.isStopped = false;
+				GoToPartner(mate.position);
+			}
+			//then reproduce 
+			else
+			{
+				agent.isStopped = true;
+				Reproduce();
+			}			
+		}
+
+		if (!canReproduce && mate!=null)
+		{
+			canReproduce = Vector3.Distance(transform.position, mate.position) >= r_SightDistance && m_Stats.c_Age>=r_AgeRange.x && m_Stats.c_Age >= r_AgeRange.y;
 		}
 	}
 
-	void Reproduce(Vector3 target)
+	void GoToPartner(Vector3 target)
 	{
+		//walk to
+		agent.SetDestination(target);
+	}
 
+	void CheckIfPartnerIsNear()
+	{
+		//get nearest partner then check its position
+
+		//first check the closest opposite sex partner that is the same age
+
+
+
+		//have it as your mate and check its position until its in sight range
+	}
+
+	void Reproduce()
+	{
+		r_Timer += Time.deltaTime;
+		if(r_Timer >= r_Duration)
+		{
+			Instantiate(baby, transform.position, Quaternion.identity);
+
+			//once reproduction is done set timer to 0 and reproduce again
+			r_Timer = 0;
+			currentBabies++;
+			r_CoolDown = 0;
+		}
 	}
 }
---
+++ Immortal Protector/Assets/Scripts/PopulationManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour {
+
+	public List<HumanCurrentStats> humans;
+
+	private void Start()
+	{
+		GameObject[] GO = GameObject.FindGameObjectsWithTag("HumanMale");
+		foreach(GameObject g in GO)
+		{
+			humans.Add(g.GetComponent<HumanCurrentStats>());
+		}
+		GameObject[] GO1 = GameObject.FindGameObjectsWithTag("HumanFemale");
+		foreach (GameObject g in GO1)
+		{
+			humans.Add(g.GetComponent<HumanCurrentStats>());
+		}
+	}
+
+
+}
