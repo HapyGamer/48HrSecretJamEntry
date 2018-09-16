1b87b44600ed73edde73cea6802c5ad54f2aa31f
Callum Scholes <hapymailer74@gmail.com>
2018-09-16 19:26:35 +1000

    added shop music

--- Immortal Protector/Assets/Script/OpenShop.cs
+++ Immortal Protector/Assets/Script/OpenShop.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class OpenShop : MonoBehaviour {
 
-	public GameObject shop;	
-	
+	public GameObject shop;
+	public AudioClip shopMusic;
+	public AudioClip normalTheme;
+	public AudioSource source;
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Escape))
@@ -15,6 +18,8 @@ public class OpenShop : MonoBehaviour {
 			{
 				Cursor.lockState = CursorLockMode.None;
 				Cursor.visible = true;
+				source.clip = shopMusic;
+				source.Play();
 				Time.timeScale = 0;
 				Camera.main.gameObject.GetComponent<CameraControl>().enabled = false;
 			}
@@ -22,6 +27,8 @@ public class OpenShop : MonoBehaviour {
 			{
 				Cursor.lockState = CursorLockMode.Locked;
 				Cursor.visible = false;
+				source.clip = normalTheme;
+				source.Play();
 				Time.timeScale = 1;
 				Camera.main.gameObject.GetComponent<CameraControl>().enabled = true;
 			}
